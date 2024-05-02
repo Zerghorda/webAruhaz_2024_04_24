e6a974a33876ae93371165d688fe8ddb0a7ef314
Tar Gergő <tar.gergo21@gmail.com>
2024-05-02 09:30:52 +0200

    click event elkezdése

--- termekek.js
+++ termekek.js
@@ -1,6 +1,6 @@
 export const termekekLISTA = [
-  { nev: "Excalibur", kep: "./kepek/excalibur.jpg", leiras: "", ar: 200 },
-  {},
-  {},
-  {},
+  { nev: "Excalibur", kep: "./kepek/excalibur.jpg", leiras: "Legendás kard", ar: 200 },
+  {nev: "Excalibur", kep: "./kepek/excalibur.jpg", leiras: "Legendás kard", ar: 200},
+  {nev: "Excalibur", kep: "./kepek/excalibur.jpg", leiras: "Legendás kard", ar: 200},
+  {nev: "Excalibur", kep: "./kepek/excalibur.jpg", leiras: "Legendás kard", ar: 200},
 ];
