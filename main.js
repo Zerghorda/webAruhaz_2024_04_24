567f1433cbe65151a389f26263d788a0fd7936c8
Tar Gergő <tar.gergo21@gmail.com>
2024-05-02 09:30:27 +0200

    kártyák meg jelennek ,kosárba tevés elkezdése

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 import { megjelenites, kartyakOsszeAllit } from "./fuggvenyek.js";
 import { termekekLISTA } from "./termekek.js";
 let nevIrany = 1;
+const articleELEM = $("article");
+const gombElem = $(".kosarba");
+const asideELEM = $("aside");
 init(termekekLISTA);
+let kosarLista = {};
+kosarbaTesz();
+function kosarbaTesz() {
+  gombElem.click( function (event) {
+    let index = 
+    $(kosarLista).append(event.taget);
+  });
+  console.log(kosarLista);
+}
 //szuresEsemeny();
 //adminElemHozzad(termekekLISTA);
 
 export function init(lista) {
-    console.log("hello");
   let txt = kartyakOsszeAllit(lista);
   megjelenites(txt, $("article"));
 }
